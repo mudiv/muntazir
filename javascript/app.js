5c8c544598df28978df4833f733ea813e6584b68
muntazir halim <86202351+mudiv@users.noreply.github.com>
2023-12-03 19:27:00 +0300

    Add files via upload

---
+++ javascript/app.js
@@ -0,0 +1,18 @@
+
+
+// document.addEventListener('DOMContentLoaded', function () {
+
+//     var dataContainer = document.getElementById('data-container');
+//     dataContainer.innerHTML += ` <div class="GameCard">
+//     <div class="gameImage">
+//         <img src="https://raw.githubusercontent.com/mudiv/quran-karim/main/image/Framelog.png">
+//     </div>
+//     <div class="gameInfo" style="text-align: right;">
+//         <p class="game-category">لعبة مصنوعة باستخدام المثلثات فقط .</p><br>
+//         <strong class="gameTitle">
+//             <span>فقط مثلثات</span>
+//             <a href="JustTrianglesAr.html" class="PlayNow">تحميل التطبيق</a>
+//         </strong>
+//     </div>
+//     </div>`;
+// });
